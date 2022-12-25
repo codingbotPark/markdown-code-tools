f5a038319c52bfa2dcb2315afcc46f35f0e1b467
박병관 <85085375+codingbotPark@users.noreply.github.com>
2022-12-25 17:50:28 +0900

    [FIX] deleting bug

--- src/service/StrongerFn.ts
+++ src/service/StrongerFn.ts
@@ -1,21 +1,43 @@
 import * as vscode from "vscode"
 
+interface CodeArrayType{
+  startLine:number;
+  endLine:number;
+  data:string;
+  type:string;
+}
+
+function getCodesInMd(editor:vscode.TextEditor){
+  let codeArrays:CodeArrayType[] = []
+
+  // 파일의 라인을 돌린다
+  for(let i = 0;i<editor.document.lineCount;i++){
+    let temp = editor.document.lineAt(i).text+"\n"
+    
+  }
+
+}
+
 /**
  * 현재 커서가 있는 마크다운 상의 코드 (```)를 인식,
  * 인식했다면 ```가 시작하는지점, 끝나는 지점을 리턴
  * 만약 커서가 있는 곳이 마크다운 상 코드가 아니라면 -1을 return
  */
-function recognizeCodeInMD(start: vscode.Position, end: vscode.Position){
+function recognizeCodeInMD(start: vscode.Position, end: vscode.Position,editor:vscode.TextEditor){
     
 }
 
 // 선택된 마크다운 형식의 코드에서 커서 블록된 라인을 아래에 만들어준다
 export function makeStrongCodeFn(start: vscode.Position, end: vscode.Position, editor: vscode.TextEditor){
     let insertedArr:string[] = []
-    
-    
-    vscode.window.showInformationMessage("start"+start.line.toString())
-    vscode.window.showInformationMessage("end"+end.line.toString())
+
+
+    // vscode.window.showInformationMessage(editor.document.lineAt(editor.document.lineCount - 1).text)
+
+    // vscode.window.showInformationMessage(editor.document.lineCount.toString())
+
+    // vscode.window.showInformationMessage("start"+start.line.toString())
+    // vscode.window.showInformationMessage("end"+end.line.toString())
 
     // 넣을 곳
     // let insertPosition = new vscode.Position() 
--- src/service/lineFn.ts
+++ src/service/lineFn.ts
@@ -24,17 +24,23 @@ export function removeLineNumberFn(start: vscode.Position, end: vscode.Position,
 	let invalidRange = new vscode.Range(start,end);
 	let replacedArr:string[] = []
 
+	// configure으로 설정된 값 가져오기
+	const gapOfLine:string = vscode.workspace.getConfiguration("markdown-code-tools").get('gapOfLineNumber')!
+
 	for(let i = start.line
 		;i<=end.line;
 		i++){
+			// 현재 라인
 			const curText = editor.document.lineAt(i).text
-			// 등록된 configure로 split하여서 나눈다
-			const curTextArr:string[] = curText.split(`${vscode.workspace.getConfiguration("markdown-code-tools").get('gapOfLineNumber')}`)
+
+			// 등록된 configure로 split하여서 나눈다, limit을 2로 준다
+			const curTextArr:string[] = curText.split(`${gapOfLine}`,2)
+
 			// 삭제하려는 라인에 라인넘버가 없는 상태일 수 있기 때문에 필터링
-			if (!!Number(curText[0])){ // 만약 숫자만 숫자만 나온다면 true
-				// 앞의 라인 넘버만 삭제하고 다시 넣어준다
-				curTextArr.shift()
-				replacedArr.push(`${curTextArr.join("")}${i===end.line?"":"\n"}`)
+			// 만약 숫자만 숫자만 나온다면 true
+			if (!!Number(curTextArr[0])){ 
+				// 앞의 라인넘버 가 있었던 길이를 삭제
+				replacedArr.push(`${curText.substring(curText[0].length+gapOfLine.length)}${i===end.line?"":"\n"}`)
 
 			} else { // false일 땐 그대로 현재 문장을 넣어준다
 				replacedArr.push(`${editor.document.lineAt(i).text}${i===end.line?"":"\n"}`)
